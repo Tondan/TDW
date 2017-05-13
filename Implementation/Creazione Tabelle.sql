26ca7fffa7f5b22ce7d52c9361858eb622185186
ChrisMatto <christian-toni@hotmail.it>
2017-05-13 11:06:03 +0200

    Ciccu

--- Implementation/Creazione Tabelle.sql
+++ Implementation/Creazione Tabelle.sql
@@ -25,8 +25,7 @@ CREATE TABLE Servizio
 
 CREATE TABLE Header
 (
-	ID_Header INT AUTO_INCREMENT PRIMARY KEY,
-    Nome VARCHAR(20) NOT NULL
+    Nome VARCHAR(20) PRIMARY KEY
 );
 
 CREATE TABLE Immagine
@@ -37,14 +36,16 @@ CREATE TABLE Immagine
 
 CREATE TABLE Slider
 (
-	ID_Slider INT auto_increment PRIMARY KEY,
-    Img BLOB
+	ID_Slider INT PRIMARY KEY,
+    Img BLOB,
+		FOREIGN KEY(Img)
+			REFERENCES Immagine(Nome_Img)
 );
 
 CREATE TABLE Home
 (
-	ID_Home INT auto_increment PRIMARY KEY,
-    Titolo VARCHAR(20),
+	ID_Home INT PRIMARY KEY,
+    Titolo VARCHAR(20) UNIQUE NOT NULL,
     Descrizione TEXT,
     Img BLOB,
 		FOREIGN KEY(Img)
@@ -53,7 +54,7 @@ CREATE TABLE Home
 
 CREATE TABLE Gallery
 (
-	ID_Gallery INT auto_increment PRIMARY KEY,
+	ID_Gallery INT PRIMARY KEY,
     Img BLOB,
 		FOREIGN KEY(Img)
 			REFERENCES Immagine(Nome_Img)
@@ -90,27 +91,42 @@ CREATE TABLE Piatto
 
 CREATE TABLE Menu
 (
-	ID_Menu INT auto_increment PRIMARY KEY,
+	Nome_Menu VARCHAR(20) PRIMARY KEY,
     Piatto VARCHAR(20),
 		FOREIGN KEY (Piatto)
 			REFERENCES Piatto(Nome_Piatto),
 	Img BLOB,
 		FOREIGN KEY(Img)
 			REFERENCES Immagine(Nome_Img)
-)
+);
 
+CREATE TABLE Prenotazione
+(
+	ID_Prenotazione INT auto_increment PRIMARY KEY,
+	Nome_Cl VARCHAR(20) NOT NULL,
+    Data TIMESTAMP NOT NULL,
+    Evento INT,
+		FOREIGN KEY (Evento)
+			REFERENCES Evento(ID_Evento),
+	N_Presone INT NOT NULL,
+);
+    
 CREATE TABLE Rist_Pizz
 (
 	ID_Rist_Pizz INT PRIMARY KEY,
     Descrizione TEXT,
 	Menu INT,
 		FOREIGN KEY (Menu)
-			REFERENCES Menu(ID_Menu)
+			REFERENCES Menu(ID_Menu),
+	Prenotazione INT,
+		FOREIGN KEY (Prenotazione)
+			REFERENCES Prenotazione(ID_Prenotazione)
 );
 
 CREATE TABLE FAQ
 (
-	Domanda VARCHAR(50) PRIMARY KEY,
+	ID_FAQ INT PRIMARY KEY,
+	Domanda VARCHAR(50) NOT NULL,
 	Risposta TEXT NOT NULL
 );
 
@@ -122,6 +138,40 @@ CREATE TABLE Evento
     Descrizione TEXT,
     Menu INT,
 		FOREIGN KEY (Menu)
-			REFERENCES Menu(ID_Menu)
+			REFERENCES Menu(ID_Menu),
+	Prenotazione INT,
+		FOREIGN KEY (Prenotazione)
+			REFERENCES Prenotazione(ID_Prenotazione)
 );
 
+CREATE TABLE Contatti
+(
+	ID_Contatti INT PRIMARY KEY,
+    Descrizione TEXT,
+    Indirizzo_Az VARCHAR(30),
+    Tel_Az VARCHAR(16),
+    E_Mail_Az VARCHAR(30),
+    Dscrizione TEXT,
+    E_Mail_Cl VARCHAR(30),
+    Nome_Cl VARCHAR(20),
+    Oggetto_Msg VARCHAR(20),
+    Messaggio TEXT
+);
+
+
+CREATE TABLE Orario
+(
+	Giorno VARCHAR(10) PRIMARY KEY,
+    Orario TIME,
+    Chiuso VARCHAR(20)
+);
+
+CREATE TABLE Footer
+(
+	ID_Footer INT PRIMARY KEY,
+    Orario VARCHAR(10),
+		FOREIGN KEY(Orario)
+			REFERENCES Orario(Giorno),
+	Nome_Az VARCHAR(20),
+    P_IVA VARCHAR(11)
+);
