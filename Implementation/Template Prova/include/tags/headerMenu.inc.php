a1c29ff6547e391727a7a62a89e5c9a7f41be8fc
Tony D'Angelo <tunyx7@gmail.com>
2017-05-24 16:09:23 +0200

    fase 2

---
+++ Implementation/Template Prova/include/tags/headerMenu.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+	Class headerMenu extends TagLibrary {
+		
+		function dummy() {
+			
+		}
+		
+		function getmenu($name, $data, $pars) {
+			
+			$menu = new Template("template/{$pars['template']}.html");
+			
+			if (!isset($pars['parent'])) {
+				$pars['parent'] = $data;
+			}
+			
+			$oid = mysql_query("SELECT * FROM header");
+			if (!$oid) {
+				trigger_error("Menu error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$menu->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+			
+			return $menu->get();
+		}
+		
+	}
