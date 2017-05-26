c0691a15edfb8e5a85076acda36e8aa122cde153
ChrisMatto <christian-toni@hotmail.it>
2017-05-26 09:58:10 +0200

    HomeBody

---
+++ Implementation/Template Prova/include/tags/HomeBody.inc.php
@@ -0,0 +1,91 @@
+<?php
+
+
+    Class HomeBody extends TagLibrary {
+		
+		function dummy() {
+			
+		}
+		
+		function getSlider($name, $data, $pars) {
+			
+			$menu = new Template("template/{$pars['template']}.html");
+			
+			if (!isset($pars['parent'])) {
+				$pars['parent'] = $data;
+			}
+			
+              
+			$oid = mysql_query("SELECT * FROM Slider");
+			if (!$oid) {
+				trigger_error("Menu error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$menu->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+			
+			return $menu->get();
+		}
+		
+        function getHome($name, $data, $pars) {
+			
+			$menu = new Template("template/{$pars['template']}.html");
+			
+			if (!isset($pars['parent'])) {
+				$pars['parent'] = $data;
+			}
+			
+              
+			$oid = mysql_query("SELECT Titolo,Descrizione FROM Home");
+			if (!$oid) {
+				trigger_error("Menu error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$menu->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+			
+			return $menu->get();
+		}
+        
+        function getImg($name, $data, $pars) {
+			
+			$menu = new Template("template/{$pars['template']}.html");
+			
+			if (!isset($pars['parent'])) {
+				$pars['parent'] = $data;
+			}
+			
+              
+			$oid = mysql_query("SELECT Img FROM Home");
+			if (!$oid) {
+				trigger_error("Menu error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$menu->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+			
+			return $menu->get();
+		}
+        
+	}
