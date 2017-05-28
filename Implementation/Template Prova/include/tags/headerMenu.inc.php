e4ba5769c746cfed8288931b1839275b6a85385b
ChrisMatto <christian-toni@hotmail.it>
2017-05-28 11:25:53 +0200

    rffft

--- Implementation/Template Prova/include/tags/headerMenu.inc.php
+++ Implementation/Template Prova/include/tags/headerMenu.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    Class headerMenu extends TagLibrary {
+    class headerMenu extends TagLibrary {
 		
 		function dummy() {
 			
@@ -20,17 +20,16 @@
 			if (!$oid) {
 				trigger_error("Menu error");
 			}
-			
+        
 			do {
 				$data = mysql_fetch_array($oid);
-				
 				if ($data) {
 					$menu->setContent($data);
 					//$menu->setContent("link", $data['link']);
 					//$menu->setContent("entry", $data['entry']);
 				}
 			} while ($data);
-			
+            
 			return $menu->get();
 		}
 		
