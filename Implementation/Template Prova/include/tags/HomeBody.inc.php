c942190ecff550ec977c5eb4cd26a37c0616fa88
ChrisMatto <christian-toni@hotmail.it>
2017-07-12 16:31:05 +0200

    Testimonials

--- Implementation/Template Prova/include/tags/HomeBody.inc.php
+++ Implementation/Template Prova/include/tags/HomeBody.inc.php
@@ -212,6 +212,33 @@
             }*/
             return $littlegll->get();
         }
+        
+        function gettestimonials($name, $data, $pars) {
+			
+			$testimonial = new Template("html/{$pars['template']}.html");
+			
+			if (!isset($pars['parent'])) {
+				$pars['parent'] = $data;
+			}
+			
+              
+			$oid = mysql_query("SELECT * FROM tdw.Recensione");
+			if (!$oid) {
+				trigger_error("Menu error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$testimonial->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+            
+			return $testimonial->get();
+		}
     }
 
 
