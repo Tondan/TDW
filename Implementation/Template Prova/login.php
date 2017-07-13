a37a8003532143dfde729dad652b4be2228c4761
tondan <tunyx7@gmail.com>
2017-07-13 22:41:35 +0200

    bla bla

--- Implementation/Template Prova/login.php
+++ Implementation/Template Prova/login.php
@@ -1,8 +1,7 @@
 <?php
-/*
-session_start();     solo nel php di backoffice
-require "include/auth.inc.php";
-*/
+
+//session_start();                //solo nel php di backoffice
+//require "include/auth.inc.php";
 
 require ("include/template2.inc.php");
 require ("include/dbms.inc.php");
