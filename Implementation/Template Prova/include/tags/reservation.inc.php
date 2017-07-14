15c0c5b2f5d810129124250b61406d124a4b796a
ChrisMatto <christian-toni@hotmail.it>
2017-07-14 18:00:54 +0200

    reservation

---
+++ Implementation/Template Prova/include/tags/reservation.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+class reservation extends TagLibrary {
+		
+		function dummy() {
+			
+		}
+        
+        function getreservation($name, $data, $pars) {
+			
+			$res = new Template("html/{$pars['template']}.html");
+			
+			if (!isset($pars['parent'])) {
+				$pars['parent'] = $data;
+			}
+			
+              
+			$oid = mysql_query("SELECT * FROM tdw.Presentazione_Reservation");
+			if (!$oid) {
+				trigger_error("Menu error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$res->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+            
+			return $res->get();
+		}
+
+}
---
+++ Implementation/Template Prova/reservation_add.php
@@ -0,0 +1,35 @@
+<?php
+
+require "include/template2.inc.php";
+require "include/dbms.inc.php";
+
+
+     
+     
+$data      = $_POST['date'];
+$persone   = $_POST['persone'];
+$tel       = $_POST['phone'];
+$ora       = $_POST['time'];
+$nome      = $_POST['name'];
+$email     = $_POST['email'];
+$note      = $_POST['notice'];
+
+echo $data;
+/*
+echo $nome,"<br>\n";
+echo $email,"<br>\n";
+echo $data,"<br>\n";
+echo $ora,"<br>\n";
+*/
+$oid = mysql_query("INSERT INTO Prenotazione (ID_Prenotazione,Data,Persone,Time,Nome_Cl,Tel,Email,Notice) 
+                    VALUES (NULL, '$data', '$persone', '$ora', '$nome', '$tel', '$email', '$note')");
+if($oid){
+	header("location: index.php?page=booking&result=ok");
+    echo("<br>prenotazione avvenuta correttamente");
+} 
+else{
+    echo("<br>Inserimento non eseguito");
+}
+
+    
+?>
