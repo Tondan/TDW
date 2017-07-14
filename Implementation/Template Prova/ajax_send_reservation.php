dc8fd37dd4a4c0c09e4f2170c9ce0edc1b1cf146
tondan <tunyx7@gmail.com>
2017-07-14 19:02:19 +0200

    SIIIIIIIIIIIIIIIIIII

--- Implementation/Template Prova/ajax_send_reservation.php
+++ Implementation/Template Prova/ajax_send_reservation.php
@@ -21,10 +21,10 @@ echo $email,"<br>\n";
 echo $data,"<br>\n";
 echo $ora,"<br>\n";
 */
-$oid = mysql_query("INSERT INTO Prenotazione (ID_Prenotazione,Data,Persone,Time,Nome_Cl,Tel,Email,Notice) 
+$oid = mysql_query("INSERT INTO tdw.prenotazione (ID_Prenotazione,Data,Persone,Time,Nome_Cl,Tel,Email,Notice) 
                     VALUES (NULL, '$data', '$persone', '$ora', '$nome', '$tel', '$email', '$note')");
 if($oid){
-	header("location:index.php");
+	header("location:home.php");
     echo("<br>prenotazione avvenuta correttamente");
 } 
 else{
