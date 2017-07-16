6063385cd2d7b8bbd44349982eee6cf49c4f3dc0
ChrisMatto <christian-toni@hotmail.it>
2017-07-16 15:44:59 +0200

    faq

---
+++ Implementation/Template Prova/faq.php
@@ -0,0 +1,17 @@
+<?php
+/*
+session_start();     solo nel php di backoffice
+require "include/auth.inc.php";
+*/
+
+require ("include/template2.inc.php");
+require ("include/dbms.inc.php");
+
+
+$main = new Template("html/index.html");
+$body = new Template("html/faq.html");
+
+$main->setContent("body", $body->get());
+$main->close();
+
+?>
