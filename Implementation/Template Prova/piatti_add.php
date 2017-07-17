6f3208a0a56b94650370ce3846944636d36bfe86
tondan <tunyx7@gmail.com>
2017-07-17 13:21:41 +0200

    okay funzia
    però gli accordion sono da aggiustare e bisogna aggiungere il campo id a piatto pero fare delete ed update

---
+++ Implementation/Template Prova/piatti_add.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+
+require "include/dbms.inc.php";
+require "include/auth1.inc.php";
+
+/*
+$nome_file_temporaneo = $_FILES['file_inviato']['tmp_name'] ;
+$nome_file = $_FILES["file_inviato"]["name"];
+$tipo_file = $_FILES["file_inviato"]["type"];*/
+$nome          = $_POST['nome'];
+$descrizione   = $_POST['descrizione'];
+$ingredienti   = $_POST['ingredienti'];
+$prezzo         =$_POST['prezzo'];
+$orario_pasto  = $_POST['tipo'];
+$presentazione = $_POST['presentazione'];
+$piattodelgiorno = $_POST['piattodelgiorno'];
+
+
+echo $nome,"<br>\n";
+echo $descrizione,"<br>\n";
+echo $ingredienti,"<br>\n";
+echo $prezzo,"<br>\n";
+echo $orario_pasto,"<br>\n";
+echo $presentazione,"<br>\n";
+echo $piattodelgiorno,"<br>\n";
+
+/*
+if(file_exists($nome_file_temporaneo)){
+	$percorso="skins/images/piatti/".$nome_file;
+	move_uploaded_file($nome_file_temporaneo,$percorso);*/
+
+$oid = mysql_query(" INSERT INTO  tdw.piatto (Nome_piatto, Descrizione, Ingredienti, Prezzo) VALUES ('$nome',  '$descrizione',  '$ingredienti', '$prezzo');");
+    //}
+/*
+else{
+	$oid = mysql_query(" 
+				INSERT INTO  `progetto`.`piatti` (
+							`ID` , 
+							`nome` , 
+							`descrizione` , 
+							`ingredienti` , 
+							`prezzo` , 
+							`orario_pasto` , 
+							`flag_presentazione` , 
+							`flag_del_giorno` )
+				VALUES ( 
+							NULL ,  
+							'$nome',  
+							'$descrizione',  
+							'$ingredienti',  
+							'$prezzo' ,
+							'$orario_pasto',  
+							'$presentazione' ,     
+							'$piattodelgiorno' );
+
+ ");
+
+}
+ 
+*/
+if($oid){
+    echo("<br>Inserimento avvenuto correttamente");
+    header("location: piatti_manager.php");
+} else{
+    echo("<br>Inserimento non eseguito<br>");
+     echo("Errore Numero: ".mysql_errno()." - Descrizione: ".mysql_error());
+  }
+  
+?>
+
