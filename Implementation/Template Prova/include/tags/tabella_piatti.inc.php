9038c45a23551c608e23a9b8ab97449b268e3ecb
ChrisMatto <christian-toni@hotmail.it>
2017-07-18 23:28:16 +0200

    SIGNOLA C'E' UN UOMO PEL LEI
    CHI E'?
    UN UOMO!

--- Implementation/Template Prova/include/tags/tabella_piatti.inc.php
+++ Implementation/Template Prova/include/tags/tabella_piatti.inc.php
@@ -16,17 +16,15 @@
 			if (!$oid) {
 				trigger_error("Menu error");
 			}
-			$i=0;
+			
 			do {
 				$data = mysql_fetch_array($oid);
 				
 				if ($data) {
 					$menu->setContent($data);
-                    $menu->setContent("id",$i);
 					//$menu->setContent("link", $data['link']);
 					//$menu->setContent("entry", $data['entry']);
 				}
-                i++;
 			} while ($data);
 			
 			return $menu->get();
