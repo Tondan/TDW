a672929e125e373cad0899e814afd2cc6d73495a
tondan <tunyx7@gmail.com>
2017-07-19 12:10:12 +0200

    delete messo, aggiustato update

---
+++ Implementation/Template Prova/include/tags/tabella_eventi.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+	Class tabella_eventi extends TagLibrary {
+		
+		function dummy() {
+			
+		}
+		
+		function getmenu($name, $data, $pars) {
+			
+			$menu = new Template("html/{$pars['template']}.html");
+			
+			
+			
+			$oid = mysql_query("SELECT * FROM tdw.evento");
+			if (!$oid) {
+				trigger_error("evento error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$menu->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+			
+			return $menu->get();
+		}
+		
+	}
--- Implementation/Template Prova/piatti_update.php
+++ Implementation/Template Prova/piatti_update.php
@@ -5,10 +5,7 @@ session_start();
 require "include/dbms.inc.php";
 require "include/auth1.inc.php";
 
-/*
-$nome_file_temporaneo = $_FILES['file_inviato']['tmp_name'] ;
-$nome_file   = $_FILES["file_inviato"]["name"];
-$tipo_file   = $_FILES["file_inviato"]["type"];*/
+
 $nome          = mysql_real_escape_string($_POST['nome']);
 $descrizione   = mysql_real_escape_string($_POST['descrizione']);
 $ingredienti   = mysql_real_escape_string($_POST['ingredienti']);
@@ -16,6 +13,10 @@ $prezzo        = mysql_real_escape_string($_POST['prezzo']);
 $tipo_piatto   = mysql_real_escape_string($_POST['pasto']);
 $img           = mysql_real_escape_string($_POST['img']);
 $key           = mysql_real_escape_string($_POST['key']);
+
+$switch = $_POST['switch'];  //radio button
+
+
 /*
 echo $nome,"<br>\n";
 echo $descrizione,"<br>\n";
@@ -23,11 +24,10 @@ echo $ingredienti,"<br>\n";
 echo $prezzo,"<br>\n";
 echo $orario_pasto,"<br>\n";
 echo $presentazione,"<br>\n";
-echo $piattodelgiorno,"<br>\n";
+echo $piattodelgiorno,"<br>\n";*/
 
-if(file_exists($nome_file_temporaneo)){
-    $percorso="skins/images/piatti/".$nome_file;
-    move_uploaded_file($nome_file_temporaneo,$percorso);*/
+
+if($switch=="update"){   
     
     $imdb= mysql_query("SELECT Link FROM tdw.Immagine WHERE Link='$img'");
     if(mysql_num_rows($imdb)==1)
@@ -35,36 +35,34 @@ if(file_exists($nome_file_temporaneo)){
     else{
         $oid = mysql_query(" INSERT INTO tdw.Immagine(Link) VALUES ('$img')");
         if($oid)
-            $oid = mysql_query(" UPDATE  tdw.piatto SET  Nome_Piatto =  '$nome',    Descrizione =  '$descrizione', Ingredienti = '$ingredienti',        Prezzo =  '$prezzo', Img= '$img', tipo_piatto='$tipo_piatto'        WHERE Nome_Piatto ='$key';");
+            $oid = mysql_query(" UPDATE  tdw.piatto SET  Nome_Piatto =  '$nome',    Descrizione =  '$descrizione', Ingredienti = '$ingredienti', Prezzo =  '$prezzo', Img= '$img', tipo_piatto='$tipo_piatto' WHERE Nome_Piatto ='$key';");
     }
-// }
-/*
-else{
-    $oid = mysql_query(" 
-         UPDATE  `progetto`.`piatti` SET  
-                 `nome` =  '$nome',
-                 `descrizione` =  '$descrizione',
-                 `ingredienti` =  '$ingredienti',
-                 `prezzo` =  '$prezzo',
-                 `orario_pasto` =  '$orario_pasto',
-                 `flag_presentazione` =  '$presentazione',
-                 `flag_del_giorno` =  '$piattodelgiorno' 
-                 WHERE  `piatti`.`ID` ='$id';
-   ");
 
-
-}
-*/
 
 
 
 
 if($oid){
-    echo("<br>Inserimento avvenuto correttamente");
-    header("location: piatti_manager.php");
+    echo("<br>update avvenuto correttamente");
+    header("location:piatti_manager.php");
 } else{
 
     echo("Errore Numero: ".mysql_errno()." - Descrizione: ".mysql_error());
   }
 
+    
+        } //fine switch==1  
+
+
+else if($switch=="delete"){
+    
+    $idd = mysql_query("DELETE FROM tdw.piatto WHERE Nome_Piatto='$key'");
+       
+if($idd){
+    echo("<br>delete avvenuto correttamente");
+    header("location: piatti_manager.php");
+} else{
+    echo("Errore Numero: ".mysql_errno()." - Descrizione: ".mysql_error());
+  }
+}     
 ?>
