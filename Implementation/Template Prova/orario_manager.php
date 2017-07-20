30db548d63bae6399a0835836250624d255d117c
tondan <tunyx7@gmail.com>
2017-07-20 18:17:28 +0200

    puttana la merda

--- Implementation/Template Prova/orario_manager.php
+++ Implementation/Template Prova/orario_manager.php
@@ -1,12 +1,12 @@
 <?php
-/*
-session_start();     solo nel php di backoffice
-require "include/auth.inc.php";
-*/
+
+session_start();     
+
+
 
 require ("include/template2.inc.php");
 require ("include/dbms.inc.php");
-
+require "include/auth1.inc.php";
 
 $main = new Template("html/indexin.html");
 $body = new Template("html/orario_manager.html");
