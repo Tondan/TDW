456e9700597316ff26e095368e5ad916c8309c3f
ChrisMatto <christian-toni@hotmail.it>
2017-07-20 18:42:06 +0200

    orario

--- Implementation/Template Prova/orario_manager.php
+++ Implementation/Template Prova/orario_manager.php
@@ -6,7 +6,7 @@ session_start();
 
 require ("include/template2.inc.php");
 require ("include/dbms.inc.php");
-require "include/auth1.inc.php";
+require ("include/auth1.inc.php");
 
 $main = new Template("html/indexin.html");
 $body = new Template("html/orario_manager.html");
--- Implementation/Template Prova/orario_update.php
+++ Implementation/Template Prova/orario_update.php
@@ -5,29 +5,20 @@ session_start();
 require "include/dbms.inc.php";
 require "include/auth1.inc.php";
 
-
-$orario          = mysql_real_escape_string($_POST['orario']);
-$giorno          = mysql_real_escape_string($_POST['giorno']);
-
-
-
-
-
-
-echo $orario,"<br>\n";
-echo $giorno,"<br>\n";
-
-
-   
-    $oid= mysql_query("UPDATE tdw.orario SET Orario='$orario' WHERE Giorno='$giorno'");
-
-
+for($i=1;$i<=7;$i++){
+$orario          = mysql_real_escape_string($_POST['orario'.$i]);
+$giorno          = mysql_real_escape_string($_POST['giorno'.$i]);
+  
+    $oid= mysql_query("UPDATE tdw.orario SET Orario='$orario' WHERE ID_Giorno='$i'");
+    if(!$oid)
+        break;
+}
 
 
 
 if($oid){
     echo("<br>update avvenuto correttamente");
-    header("refresh:5; admin.php");
+    header("refresh:2; admin.php");
 } else{
 
     echo("Errore Numero: ".mysql_errno()." - Descrizione: ".mysql_error());
