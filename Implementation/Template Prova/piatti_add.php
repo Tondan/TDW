435d4a61d0ba84dd88306e3d74acdce2f9ebfccf
tondan <tunyx7@gmail.com>
2017-07-20 20:51:59 +0200

    FUNZIAAAAAAA
    manca sull'update però u.u

--- Implementation/Template Prova/piatti_add.php
+++ Implementation/Template Prova/piatti_add.php
@@ -4,14 +4,13 @@ session_start();
 
 require "include/dbms.inc.php";
 require "include/auth1.inc.php";
-
+require "upload_in.php";
 
 $nome          = mysql_real_escape_string($_POST['nome']);
 $descrizione   = mysql_real_escape_string($_POST['descrizione']);
 $ingredienti   = mysql_real_escape_string($_POST['ingredienti']);
 $prezzo        = mysql_real_escape_string($_POST['prezzo']);
 $tipo_piatto   = mysql_real_escape_string($_POST['tipo_piatto']);
-$img           = mysql_real_escape_string($_POST['img']);
 
 
 
@@ -26,7 +25,7 @@ echo $img,"<br>\n";*/
 
 $imdb= mysql_query("SELECT Link FROM tdw.Immagine WHERE Link='Img/$img'");
 if(mysql_num_rows($imdb)==1){
-    $oid = mysql_query(" INSERT INTO  tdw.piatto (Nome_piatto, Descrizione, Ingredienti, Prezzo,Img,tipo_piatto) VALUES ('$nome',  '$descrizione',  '$ingredienti', '$prezzo', 'Img/$img', '$tipo_piatto');");}
+    $oid = mysql_query(" INSERT INTO  tdw.piatto (Nome_piatto, Descrizione, Ingredienti, Prezzo,Img,tipo_piatto) VALUES ('$nome',  '$descrizione',  '$ingredienti', '$prezzo', '$target_file', '$tipo_piatto');");}
 else{
     $oid=mysql_query(" INSERT INTO tdw.Immagine(Link) VALUES ('Img/$img')");
     if($oid)
--- Implementation/Template Prova/upload_in.php
+++ Implementation/Template Prova/upload_in.php
@@ -1,26 +1,12 @@
 <?php
 
-session_start();
-
-require "include/dbms.inc.php";
-require "include/auth1.inc.php";
 
 $target_dir = "Img/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-$pic=(mysql_real_escape_string($_FILES['photo']['name']));
-// CONTROLLO IMMAGINE
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
+
+
 // Controlla se il file esiste già
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
@@ -45,11 +31,9 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         $oid=mysql_query("INSERT INTO tdw.immagine(Link) VALUES('$target_file')");
-        if($oid)
-            header("refresh:2; admin.php");
-        else
-            echo("<br>Inserimento non avvenuto correttamente");
-        echo("Errore Numero: ".mysql_errno()." - Descrizione: ".mysql_error());
+        if(!$oid){
+        echo("<br>Inserimento immagine non avvenuto correttamente");
+        echo("Errore Numero: ".mysql_errno()." - Descrizione: ".mysql_error());}
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
