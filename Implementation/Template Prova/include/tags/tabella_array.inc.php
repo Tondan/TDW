1d812596c11090fdf4502bc76932474c1264a85f
ChrisMatto <christian-toni@hotmail.it>
2017-07-21 18:31:59 +0200

    robe

--- Implementation/Template Prova/include/tags/tabella_array.inc.php
+++ Implementation/Template Prova/include/tags/tabella_array.inc.php
@@ -14,12 +14,12 @@
 				$pars['parent'] = $data;
 			}
 			
-            $pippa=mysql_query("SELECT Link FROM tdw.Immagine WHERE SUBSTRING(Link,5,4)='food'AND Attivo=1");  
+            $pippa=mysql_query("SELECT Link FROM tdw.immagine WHERE SUBSTRING(Link,5,4)='food'AND Attivo=1");  
             if (!$oid) {
 				trigger_error("Menu error");
             }
             
-			$oid = mysql_query("SELECT Link FROM tdw.Immagine WHERE SUBSTRING(Link,5,4)='food' AND Attivo=0");
+			$oid = mysql_query("SELECT Link FROM tdw.immagine WHERE SUBSTRING(Link,5,4)='food' AND Attivo=0");
 			if (!$oid) {
 				trigger_error("Menu error");
 			}
@@ -32,7 +32,22 @@
 				$data = mysql_fetch_array($oid);
 				
 				if ($data) {
-					$food->setContent($data);
+					$food->setContent("LinkDelete",$data['Link']);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+            
+            $oid = mysql_query("SELECT Link FROM tdw.immagine WHERE SUBSTRING(Link,5,4)='food' AND Attivo=0");
+			if (!$oid) {
+				trigger_error("Menu error");
+            }
+                
+                do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$food->setContent("LinkActive",$data['Link']);
 					//$menu->setContent("link", $data['link']);
 					//$menu->setContent("entry", $data['entry']);
 				}
