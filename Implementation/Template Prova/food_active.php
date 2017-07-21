15fa292e421986f8954869816a646b2935726d1a
ChrisMatto <christian-toni@hotmail.it>
2017-07-21 18:45:21 +0200

    ecco

--- Implementation/Template Prova/food_active.php
+++ Implementation/Template Prova/food_active.php
@@ -8,12 +8,12 @@ require "include/auth1.inc.php";
 $img          = mysql_real_escape_string($_POST['switch']);
 $active       = mysql_real_escape_string($_POST['switch-active']);
 
-if($img!=""){
+if($img!=$active){
     $oid=mysql_query("UPDATE tdw.immagine SET Attivo=0 WHERE Link='$active'");
         if($oid){
             $oid=mysql_query("UPDATE tdw.immagine SET Attivo=1 WHERE Link='$img'");
             echo "Update avvenuto correttamente";
-            header("refresh:2;array-manager.php");
+            header("refresh:2;array_manager.php");
         }
     
 }
--- Implementation/Template Prova/food_add.php
+++ Implementation/Template Prova/food_add.php
@@ -7,6 +7,6 @@ require "include/auth1.inc.php";
 $dir="Img/food/";
 require "upload_in_Img.php";
 
-
+header("refresh:2; array_manager.php");
   
 ?>
