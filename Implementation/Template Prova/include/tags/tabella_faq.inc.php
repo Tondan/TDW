0cc0bd1e476ae48bc3af6bdda5f485746abf91d9
tondan <tunyx7@gmail.com>
2017-07-21 16:04:23 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Implementation/Template Prova/include/tags/tabella_faq.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+	Class tabella_faq extends TagLibrary {
+		
+		function dummy() {
+			
+		}
+		
+		function getmenu($name, $data, $pars) {
+			
+			$menu = new Template("html/{$pars['template']}.html");
+			
+			
+			
+			$oid = mysql_query("SELECT * FROM tdw.faq");
+			if (!$oid) {
+				trigger_error("error");
+			}
+			
+			do {
+				$data = mysql_fetch_array($oid);
+				
+				if ($data) {
+					$menu->setContent($data);
+					//$menu->setContent("link", $data['link']);
+					//$menu->setContent("entry", $data['entry']);
+				}
+			} while ($data);
+			
+			return $menu->get();
+		}
+		
+	}
