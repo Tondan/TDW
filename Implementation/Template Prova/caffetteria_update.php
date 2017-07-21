9772c01cdb9dab8e35394c49f6798464669a1a00
tondan <tunyx7@gmail.com>
2017-07-21 23:32:29 +0200

    caff

---
+++ Implementation/Template Prova/caffetteria_update.php
@@ -0,0 +1,52 @@
+<?php
+
+session_start();
+
+require "include/dbms.inc.php";
+require "include/auth1.inc.php";
+$dir="Img/";
+
+
+$nome          = mysql_real_escape_string($_POST['titolo']);
+$descrizione   = mysql_real_escape_string($_POST['descrizione']);
+$key           = mysql_real_escape_string($_POST['key']);
+$keyImg        = mysql_real_escape_string($_POST['keyImg']);
+
+$switch = $_POST['switch'];  //radio button
+
+
+/*
+echo $nome,"<br>\n";
+echo $descrizione,"<br>\n";
+echo $ingredienti,"<br>\n";
+echo $prezzo,"<br>\n";
+echo $orario_pasto,"<br>\n";
+echo $presentazione,"<br>\n";
+echo $piattodelgiorno,"<br>\n";*/
+
+
+
+    require "upload_in_Img.php";
+    if(strcmp($target_file,$dir)==0){
+        $target_file=$keyImg;
+        
+        $oid = mysql_query("UPDATE tdw.caffetteria SET Titolo = '$nome', Descrizione = '$descrizione', Img = '$target_file' WHERE ID_Caffetteria = '$key';");
+                                    }
+    else{
+        $oid = mysql_query(" UPDATE tdw.caffetteria SET Titolo =  '$nome', Descrizione =  '$descrizione', Img ='$target_file' WHERE ID_Caffetteria = '$key';");
+        if($oid){
+            $idd=mysql_query("DELETE FROM tdw.immagine WHERE Link='$keyImg'");
+            if($idd)
+                unlink($keyImg);}
+        }
+
+
+if($oid){
+    echo("<br>update avvenuto correttamente");
+    header("refresh:2; caffetteria_manager.php");
+} else{
+
+    echo("Errore Numero: ".mysql_errno()." - Descrizione: ".mysql_error());
+  }
+     
+?>
